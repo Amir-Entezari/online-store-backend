1b8cf449132b5b429e3bdd9335675fa106bc95eb
Amir-Entezari <mr.amirentezari@gmail.com>
2023-02-09 18:17:00 +0330

    retrieving an object using ORM

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 # Create your views here.
 
 
 def say_hello(request):
+    try:
+        product = Product.objects.get(pk=0)
+    except ObjectDoesNotExist:
+        pass
+
     return render(request, 'hello.html', {'name': 'amir'})
