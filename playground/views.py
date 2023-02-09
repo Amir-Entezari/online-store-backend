b1e4fb0697d5fa4cc22a3bc5e8f3ad2ac005e484
Amir-Entezari <mr.amirentezari@gmail.com>
2023-02-09 19:33:17 +0330

    Selecting products that have been ordered and sorting them by title

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
-from django.db.models import Q,F
+from django.db.models import Q, F
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from store.models import Product, OrderItem
 # Create your views here.
 
 
 def say_hello(request):
-    queryset = Product.objects.values('id','title','unit_price','collection__title') # search for more: queryset api
+    queryset = Product.objects.filter(id__in=OrderItem.objects.values(
+        'product_id').distinct()).order_by('title')  # search for more: queryset api
 
     return render(request, 'hello.html', {'name': 'amir', 'products': list(queryset)})
