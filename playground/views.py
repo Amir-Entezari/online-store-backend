d5283a683090c00e9271bb896e97625e76e906b5
Amir-Entezari <mr.amirentezari@gmail.com>
2023-02-09 19:21:32 +0330

    Sorting

--- playground/views.py
+++ playground/views.py
@@ -7,6 +7,6 @@ from store.models import Product
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(inventory=F('unit_price'))  # search for more: queryset api
+    queryset = Product.objects.order_by('title','unit_price')  # search for more: queryset api
 
     return render(request, 'hello.html', {'name': 'amir', 'products': list(queryset)})
