8e634ffc4e3d9bdedbfea29767fae81008fde3e3
Amir-Entezari <mr.amirentezari@gmail.com>
2023-02-09 20:55:30 +0330

    Creating objects

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
-from store.models import Product
+from store.models import Product, Collection
 from tags.models import TaggedItem
 
 
 def say_hello(request):
-    queryset = TaggedItem.objects.values('objects')
-    return render(request, 'hello.html', {'name': 'amir', 'tags': queryset})
+    collection = Collection()
+    collection.title = 'Video Games'
+    collection.featured_product = Product(pk=1)
+    collection.save()
+    # OR
+    # Collection.objects.create(title='Video Games', featured_product=Product(pk=1))
+    return render(request, 'hello.html', {'name': 'amir'})
