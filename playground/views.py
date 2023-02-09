79370fff58f6cf167d8e379ed29e3d4062cd56cc
Amir-Entezari <mr.amirentezari@gmail.com>
2023-02-09 19:49:37 +0330

    Getting the last 5 orders with their customer and items

--- playground/views.py
+++ playground/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.db.models import Q, F
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product, OrderItem
+from store.models import Product, OrderItem,Order
 # Create your views here.
 
 
 def say_hello(request):
-    queryset = Product.objects.prefetch_related('promotion').select_related('collection').all()  # search for more: queryset api
+    queryset = Order.objects.select_related('customer').prefetch_related(
+        'orderitem_set__product').order_by('-placed_at')[:5]  # search for more: queryset api
 
-    return render(request, 'hello.html', {'name': 'amir', 'products': list(queryset)})
+    return render(request, 'hello.html', {'name': 'amir', 'orders': list(queryset)})
