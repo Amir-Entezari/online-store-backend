b64e68e94e70d1a7db05c2bcd8fc1ba6c236dbde
Amir-Entezari <mr.amirentezari@gmail.com>
2023-02-09 20:19:54 +0330

    Another method for concating

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.db.models import Value, F, Func
+from django.db.models.functions import Concat
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from store.models import Product, Customer
 # Create your views here.
 
 
 def say_hello(request):
-    result = Customer.objects.annotate(full_name=Func(
-        F('first_name'), Value(' '), F('last_name'), function='CONCAT'))
+    result = Customer.objects.annotate(
+        full_name=Concat('first_name', Value(' '), 'last_name'))  # search "django database functions" for more
 
     return render(request, 'hello.html', {'name': 'amir', 'result': result})
