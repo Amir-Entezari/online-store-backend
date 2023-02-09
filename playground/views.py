3271d4602ad2ac04dc7565e5dcae59674deb6941
Amir-Entezari <mr.amirentezari@gmail.com>
2023-02-09 20:17:30 +0330

    Calling database functions using Func

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from django.db.models import Value,F
+from django.db.models import Value, F, Func
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
-from store.models import Product,Customer
+from store.models import Product, Customer
 # Create your views here.
 
 
 def say_hello(request):
-    result = Customer.objects.annotate(is_new=Value(True),new_id=F('id')+1)
+    result = Customer.objects.annotate(full_name=Func(
+        F('first_name'), Value(' '), F('last_name'), function='CONCAT'))
 
     return render(request, 'hello.html', {'name': 'amir', 'result': result})
