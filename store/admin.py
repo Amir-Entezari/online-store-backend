6098155a9256c3b6c3564d20cf299d0ca91a74bb
Amir-Entezari <mr.amirentezari@gmail.com>
2023-02-11 22:26:38 +0330

    Adding invventory_status column to ProductAdmin class

--- store/admin.py
+++ store/admin.py
@@ -10,9 +10,16 @@ class CollectionAdmin(admin.ModelAdmin):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
     list_per_page = 20
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return "Low"
+        else:
+            return "OK"
 
 
 admin.site.register(models.Promotion)
