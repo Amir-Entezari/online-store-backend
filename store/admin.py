83c405363cc211d4f8192ef45344b07e21c1a893
Amir-Entezari <mr.amirentezari@gmail.com>
2023-02-11 22:22:56 +0330

    Customizing the List Page

--- store/admin.py
+++ store/admin.py
@@ -2,12 +2,45 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-admin.site.register(models.Collection)
-admin.site.register(models.Product)
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+    list_editable = ['unit_price']
+    list_per_page = 20
+
+
 admin.site.register(models.Promotion)
-admin.site.register(models.Customer)
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 20
+
+
 admin.site.register(models.Address)
-admin.site.register(models.Order)
-admin.site.register(models.OrderItem)
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['placed_at', 'customer_id', 'payment_status']
+    ordering = ['-placed_at']
+    list_per_page = 20
+
+
+@admin.register(models.OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order_id', 'product', 'quantity', 'unit_price']
+    ordering = ['order__placed_at']
+
+
 admin.site.register(models.Cart)
 admin.site.register(models.CartItem)
