b535516af233a3694b2b8bc60af3a32ca818ab00
Amir-Entezari <mr.amirentezari@gmail.com>
2023-02-13 14:31:19 +0330

    Make description nullable in product model and blank

--- store/models.py
+++ store/models.py
@@ -23,7 +23,7 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    description = models.TextField()
+    description = models.TextField(null=True,blank=True)
     unit_price = models.DecimalField(max_digits=7, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
