4ea206dbd0c13a8c785c38d48dfaaa19d0f717e3
Amir-Entezari <mr.amirentezari@gmail.com>
2023-02-13 14:34:27 +0330

    Adding validators to unit_price and inventory field in product model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 # Create your models here.
@@ -23,12 +24,15 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    description = models.TextField(null=True,blank=True)
-    unit_price = models.DecimalField(max_digits=7, decimal_places=2)
-    inventory = models.IntegerField()
+    description = models.TextField(null=True, blank=True)
+    unit_price = models.DecimalField(
+        max_digits=7,
+        decimal_places=2,
+        validators=[MinValueValidator(1)])
+    inventory = models.IntegerField(validators=[MinValueValidator(0)])
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
-    promotion = models.ManyToManyField(Promotion)
+    promotion = models.ManyToManyField(Promotion,blank=True)
 
     def __str__(self) -> str:
         return self.title
