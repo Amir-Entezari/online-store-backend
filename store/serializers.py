831c94585fbfe838ed9106917cb3948c51dec85a
Amir-Entezari <mr.amirentezari@gmail.com>
2023-02-20 09:34:55 +0330

    Adding price_with_tax field to serializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,11 @@
+from decimal import Decimal
 from rest_framework import serializers
-
+from .models import Product
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=7,decimal_places=2)
+    price = serializers.DecimalField(max_digits=7,decimal_places=2,source='unit_price')
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+
+    def calculate_tax(self,product:Product):
+        return product.unit_price * Decimal(1.1)
