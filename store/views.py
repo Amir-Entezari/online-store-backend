8b44950f7dc79844674f092fd07c891add00dff4
Amir-Entezari <mr.amirentezari@gmail.com>
2023-02-20 16:30:30 +0330

    Adding DELETE method to product_detail

--- store/views.py
+++ store/views.py
@@ -21,17 +21,22 @@ def product_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, id):
+    product = get_object_or_404(Product, pk=id)
     if request.method == 'GET':
-        product = get_object_or_404(Product, pk=id)
-        serializer = ProductSerializer(product)
+        serializer = ProductSerializer(product, context={'request': request})
         return Response(serializer.data)
     elif request.method == 'PUT':
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid()
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        if product.orderitem_set.count() >= 1:
+            return Response({'error': 'product cannot be deleted because it is associated with an order item'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view()
