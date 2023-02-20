3a8678da0d11365b53d855909ffef502ead7f50c
Amir-Entezari <mr.amirentezari@gmail.com>
2023-02-20 15:39:03 +0330

    Adding collection field to ProductSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,11 +1,16 @@
 from decimal import Decimal
 from rest_framework import serializers
 from .models import Product
+
+
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=7,decimal_places=2,source='unit_price')
-    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    price = serializers.DecimalField(
+        max_digits=7, decimal_places=2, source='unit_price')
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='calculate_tax')
+    collection = serializers.StringRelatedField()
 
-    def calculate_tax(self,product:Product):
+    def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
