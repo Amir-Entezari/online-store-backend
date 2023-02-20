3e0b26ba170d3826dda9fc5d9e3e912a95c413f2
Amir-Entezari <mr.amirentezari@gmail.com>
2023-02-20 18:37:14 +0330

    Changing ProductDetail and CollectionDetail to inherit from Generic views

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('products/', views.ProductList.as_view()),
-    path('products/<int:id>/', views.ProductDetail.as_view()),
+    path('products/<int:pk>/', views.ProductDetail.as_view()),
     path('collections/', views.CollectionList.as_view()),
     path('collections/<int:pk>/', views.CollectionDetail.as_view(),
          name='collection-detail')
--- store/views.py
+++ store/views.py
@@ -17,22 +17,12 @@ class ProductList(ListCreateAPIView):
         return {'request': self.request}
 
 
-class ProductDetail(APIView):
+class ProductDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
-    def get(self, request, id):
-        product = get_object_or_404(Product, pk=id)
-        serializer = ProductSerializer(product, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, id):
-        product = get_object_or_404(Product, pk=id)
-        serializer = ProductSerializer(product, data=request.data)
-        serializer.is_valid()
-        serializer.save()
-        return Response(serializer.data)
-
-    def delete(self, request, id):
-        product = get_object_or_404(Product, pk=id)
+    def delete(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
         if product.orderitem_set.count() >= 1:
             return Response({'error': 'product cannot be deleted because it is associated with an order item'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
@@ -45,24 +35,13 @@ class CollectionList(ListCreateAPIView):
     serializer_class = CollectionSerializer
 
 
-class CollectionDetail(APIView):
-    def get(self, request, pk):
-        collection = get_object_or_404(Collection.objects.annotate(
-            products_count=Count('product')), pk=pk)
-        serializer = CollectionSerializer(collection)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        collection = get_object_or_404(Collection.objects.annotate(
-            products_count=Count('product')), pk=pk)
-        serializer = CollectionSerializer(collection, request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+class CollectionDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Collection.objects.annotate(
+        products_count=Count('product'))
+    serializer_class = CollectionSerializer
 
     def delete(self, request, pk):
-        collection = get_object_or_404(Collection.objects.annotate(
-            products_count=Count('product')), pk=pk)
+        collection = get_object_or_404(Collection, pk=pk)
         if collection.product_set.count() > 0:
             return Response({'error': 'collection cannot be deleted because it includes one or more products.'})
         collection.delete()
