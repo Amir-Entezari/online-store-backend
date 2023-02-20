4b3e95f52fc82600564a41cf0225836ea02e9ee5
Amir-Entezari <mr.amirentezari@gmail.com>
2023-02-20 23:08:18 +0330

    Replacing SimpleRouter with DefaultRouter that gives us additional feature(link to endpoints on Localhost/store/)

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 from . import views
 
-router = SimpleRouter()
+router = DefaultRouter()
 router.register('products', views.ProductViewSet)
 router.register('collections', views.CollectionViewSet)
 urlpatterns = [
