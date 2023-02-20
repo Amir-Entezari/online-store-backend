2ce4633473084e74ac55eb1d2ae3d8eceeee9b42
Amir-Entezari <mr.amirentezari@gmail.com>
2023-02-20 23:06:15 +0330

    BugFix - Fixing the delete method in ProductViewSet and CollectionViewSet by change it to destro

--- store/views.py
+++ store/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework import status
-from .models import Product, Collection
+from .models import Product, Collection, OrderItem
 from .serializers import ProductSerializer, CollectionSerializer
 from django.db.models.aggregates import Count
 
@@ -16,12 +16,10 @@ class ProductViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {'request': self.request}
 
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitem_set.count() >= 1:
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() >= 1:
             return Response({'error': 'product cannot be deleted because it is associated with an order item'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
 
 
 class CollectionViewSet(ModelViewSet):
@@ -29,9 +27,7 @@ class CollectionViewSet(ModelViewSet):
         products_count=Count('product')).all()
     serializer_class = CollectionSerializer
 
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        if collection.product_set.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs['pk']).count() > 0:
             return Response({'error': 'collection cannot be deleted because it includes one or more products.'})
-        collection.delete()
-        return Response(status=status.HTTP_201_CREATED)
+        return super().destroy(request, *args, **kwargs)
