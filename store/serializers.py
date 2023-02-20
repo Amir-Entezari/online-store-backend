060bbd0026c858bfdd9e32dd4495d471727b9d65
Amir-Entezari <mr.amirentezari@gmail.com>
2023-02-20 15:59:54 +0330

    Using ModelSerializer instead of Serializer in CollectionSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,17 +1,18 @@
 from decimal import Decimal
 from rest_framework import serializers
-from .models import Product,Collection
+from .models import Product, Collection
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id','title','unit_price','collection','price_with_tax']
+        fields = ['id', 'title', 'unit_price', 'collection', 'price_with_tax']
     price_with_tax = serializers.SerializerMethodField(
         method_name='calculate_tax')
     collection = serializers.HyperlinkedRelatedField(
