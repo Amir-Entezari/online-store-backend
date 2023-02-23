4bb0085c9f5d04cf7bd6345aab10580cc20432fd
Amir-Entezari <mr.amirentezari@gmail.com>
2023-02-23 19:23:58 +0330

    creating custom permissions for ordering (without functionality)

--- store/models.py
+++ store/models.py
@@ -88,6 +88,11 @@ class Order(models.Model):
         max_length=1, choices=PAYMENT_STATUS, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
+    class Meta:
+        permissions = [
+            ('cancel_order', 'Can cancel order')
+        ]
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
