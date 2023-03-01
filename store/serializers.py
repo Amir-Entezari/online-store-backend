98ec30627dc1708bb467e354fe3e9144f71799fc
Amir-Entezari <mr.amirentezari@gmail.com>
2023-03-01 12:13:13 +0330

    Returning the created order to the client

--- store/serializers.py
+++ store/serializers.py
@@ -153,3 +153,4 @@ class CreateOrderSerializer(serializers.Serializer):
             ]
             OrderItem.objects.bulk_create(order_items)
             Cart.objects.filter(pk=cart_id).delete()
+            return order
