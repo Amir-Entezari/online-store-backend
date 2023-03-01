44a1ef7e0d41b84742bbe772e001b0d7f65ce942
Amir-Entezari <mr.amirentezari@gmail.com>
2023-03-01 12:33:18 +0330

    Creating signals.py and defining create method for new user to replace get_or_create function with simple get() method

--- store/views.py
+++ store/views.py
@@ -93,7 +93,7 @@ class CustomerViewSet(ModelViewSet):
 
     @action(detail=False, methods=['GET', 'PUT'], permission_classes=[IsAuthenticated])
     def me(self, request):
-        (customer, created) = Customer.objects.get_or_create(
+        customer = Customer.objects.get(
             user_id=request.user.id)
         if request.method == 'GET':
             serializer = CustomerSerializer(customer)
@@ -132,6 +132,6 @@ class OrderViewSet(ModelViewSet):
         user = self.request.user
         if user.is_staff:
             return Order.objects.all()
-        (customer_id, created) = Customer.objects.only(
-            'id').get_or_create(user_id=user.id)
+        customer_id = Customer.objects.only(
+            'id').get(user_id=user.id)
         return Order.objects.filter(customer_id=customer_id)
