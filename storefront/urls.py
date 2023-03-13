ae5226bb660840a88265d086e7349ce6b0f64e84
Amir-Entezari <mr.amirentezari@gmail.com>
2023-03-13 12:20:06 +0330

    Managing media files - Defining setting variebles and testing

--- storefront/urls.py
+++ storefront/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 import debug_toolbar
 
 admin.site.site_header = 'Store Admin'
@@ -26,5 +28,8 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    path('__debug__/',include(debug_toolbar.urls)),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
