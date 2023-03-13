9b39dcad184d0b409fe061c8c1a3656eb1abe1c3
Amir-Entezari <mr.amirentezari@gmail.com>
2023-03-13 13:07:44 +0330

    Adding validators for image size

--- store/models.py
+++ store/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
 from uuid import uuid4
+from .validators import validate_file_size
 # Create your models here.
 
 
@@ -45,7 +46,8 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='store/images')
+    image = models.ImageField(upload_to='store/images',
+                              validators=[validate_file_size])
 
 
 class Customer(models.Model):
